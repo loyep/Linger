1d1ef7e2f4958bc534fe1899420a264c5be31ec5
Maxsey <mwl@live.com>
2018-07-26 16:35:28 +0800

    Initial project

---
+++ Linger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Linger
+//
+//  Created by Loren on 2018/7/26.
+//  Copyright © 2018年 Linger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Linger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Linger
+//
+//  Created by Loren on 2018/7/26.
+//  Copyright © 2018年 Linger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
