9f0d2d30a5c4035038f310ef848d60e61a7f12b5
Maxsey <mwl@live.com>
2018-07-30 10:08:24 +0800

    Update Xcode

---
+++ Linger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Linger
+//
+//  Created by Loren on 2018/7/30.
+//  Copyright © 2018年 Linger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
